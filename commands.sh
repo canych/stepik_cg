a36c88961a781a7100abd2b795c600ab445d7c3a
Alexander <10341843+canych@users.noreply.github.com>
2018-07-31 20:11:52 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 ffmpeg -i input_video.mp4 -ss 00:00:31 -t 00:00:10 -c copy cropped.mp4
 convert -depth 8 -background transparent -resize 50% plate.svg plate.png
 ffmpeg -i cropped.mp4 -i plate.png -filter_complex "[0:v][1:v] overlay=0:446: enable='between(t,1,9)'" -c:a copy plated.mp4
-ffmpeg -i plated.mp4 -vf "drawtext=enable='between(t,2,9)': fontfile=Arial.ttf: fontsize=30: x=200: y=476: text='Тратата'" -c:a copy result.mp4
+ffmpeg -i plated.mp4 -vf drawtext="text='Тратата':enable='between(t,2,9)': fontfile=Arial.ttf: fontsize=30: fontcolor=black: x=200: y=476" -c:a copy result.mp4
